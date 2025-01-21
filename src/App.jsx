b85f8193182be9b925ab9b238457a5c16da7b6a8
Anuj Pardeshi <anujpardeshi500@gmail.com>
2025-01-21 11:13:05 +0530

    Add new files

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Counter from './Counter'
+import LikeButton from './LikeButton'
+import LudoBoard from './ludoBoard'
+
+function App() {
+  return (
+    <>
+    {/* <h1>States in React</h1> */}
+    {/* <Counter /> */}
+    {/* <LikeButton/> */}
+    <LudoBoard/>
+    </>
+  )
+}
+
+export default App
